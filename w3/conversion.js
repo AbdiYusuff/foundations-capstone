e6cb5a906fe43c90670a2c02c347ca74b4e80274
Abdi Yusuf <ayusufadan@gmail.com>
2022-08-08 11:43:30 -0500

    capstone-draft-1

---
+++ w3/conversion.js
@@ -0,0 +1,8 @@
+
+function TpsConverter(tsp) {
+    document.getElementById("outputTbsp").innerHTML=tsp*3;
+  }
+
+function TbspConverter(tbsp) {
+    document.getElementById("outputTsp").innerHTML=tbsp*.33;
+  }
